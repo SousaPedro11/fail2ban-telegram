7e0168444a131d152e993cded098f15b50957ce1
Pedro Sousa <ppls2106@gmail.com>
2020-04-05 10:45:07 -0300

    edit auth

--- app/authorization/auth_impl.py
+++ app/authorization/auth_impl.py
@@ -1,5 +1,6 @@
 import os
 
+from werkzeug.exceptions import Unauthorized
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app.authorization import auth
@@ -12,4 +13,4 @@ __senha = os.environ.get('HTTPAUTH_PASS')
 def verify_password(username, password):
     if username == __usuario:
         return check_password_hash(generate_password_hash(__senha), password)
-    return False
+    raise Unauthorized
