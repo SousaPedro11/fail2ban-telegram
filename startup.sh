e3773a712680ec520ae3a020bdc43223300da146
Pedro Sousa <ppls2106@gmail.com>
2020-04-07 16:33:05 -0300

    ajuste de modulo

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn --workers=5 --bind=0.0.0.0:5000 --access-logfile - --error-logfile - fail2ban:app
+gunicorn --workers=5 --bind=0.0.0.0:5000 --access-logfile - --error-logfile - 'fail2ban:create_app()'
